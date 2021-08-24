e655b17c5ffd12680182e1a494bccd361c94c89f
[AkmaralKakhar] <[kahar.akmaral@gmail.com]>
2021-08-24 23:25:41 +0600

    initial commit

--- app/src/main/java/com/example/androiddevhw_2/BindingActivity.kt
+++ app/src/main/java/com/example/androiddevhw_2/BindingActivity.kt
@@ -4,14 +4,3 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androiddevhw_2.databinding.ActivityBindingBinding
 
-class BindingActivity : AppCompatActivity() {
-    //private  lateinit var binding: ActivityBindingBinding
-    private var binding: ActivityBindingBinding? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-       binding = ActivityBindingBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
-        binding!!.textForBinding
-    }
-}
--- app/src/main/java/com/example/androiddevhw_2/PdfViewActivity.kt
+++ app/src/main/java/com/example/androiddevhw_2/PdfViewActivity.kt
@@ -7,16 +7,3 @@ import android.os.Bundle
 import android.widget.Button
 import com.github.barteksc.pdfviewer.PDFView
 
-class PdfViewActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pdf_view)
-
-        findViewById<Button>(R.id.openPdfBtn)?.setOnClickListener{
-           val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.africau.edu/images/default/sample.pdf"))
-           startActivity(intent)
-       }
-
-        //findViewById<PDFView>(R.id.pdfView).fromAsset("test.pdf").load()
-    }
-}
